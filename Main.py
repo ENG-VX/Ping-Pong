731511470951fdf054263aa88c2e93d3d636f22e
ENG-VX <abdullahwork5757@gmail.com>
2024-10-27 15:10:23 +0400

    Make the movability so soft and clear for eyes

--- Main.py
+++ Main.py
@@ -1,13 +1,14 @@
-from turtle import Turtle,Screen
+from turtle import Turtle, Screen
 from paddle import Paddle
 from ball import Ball
 from score import Score
+import time
 
 window = Screen()
 window.title("Ping Pong")
-window.setup(800,600)
+window.setup(800, 600)
 window.bgcolor("black")
-
+window.tracer(0)  # تعطيل التحديث التلقائي لتسريع الأداء
 
 L_paddle = Paddle(-380)
 R_paddle = Paddle(380)
@@ -15,19 +16,22 @@ R_paddle = Paddle(380)
 window.listen()
 window.onkeypress(L_paddle.move_up, "w")
 window.onkeypress(L_paddle.move_down, "s")
+window.onkeyrelease(L_paddle.stop, "w")
+window.onkeyrelease(L_paddle.stop, "s")
 window.onkeypress(R_paddle.move_up, "Up")
 window.onkeypress(R_paddle.move_down, "Down")
+window.onkeyrelease(R_paddle.stop, "Up")
+window.onkeyrelease(R_paddle.stop, "Down")
 
 ball = Ball()
 score = Score()
-while True:
+
+def update_game():
     ball.moving(L_paddle, R_paddle, score)
     R_paddle.update()
     L_paddle.update()
-    
-    
+    window.update()  # تحديث الشاشة
+    window.ontimer(update_game, 10)  # تحديث اللعبة كل 10 ملي ثانية
 
-
-
-
+update_game()  # بدء اللعبة
 window.exitonclick()
--- ball.py
+++ ball.py
@@ -6,37 +6,44 @@ class Ball(Turtle):
         self.shape("circle")
         self.color("white")
         self.penup()
-        self.x_move = 5
-        self.y_move = 5
+        self.x_move = 1
+        self.y_move = 1
 
     def moving(self, L_paddle, R_paddle, score):
-
+        # اذا ضربت الكرة باحد الحدود الرأسية
         x,y = self.pos()
         if y>=280 or y<=-280:
             self.y_move *= -1
 
-
-            
-        if x>=380:
+        # اذا تعدت الجد الايمن    
+        if x>=415:
             score.L_scoer += 1
             score.update_score()
             self.goto(0,0)
-            self.x_move *= -1
+            self.x_move = -1
+            self.y_move = 1
 
-
-        if x<=-380:
+        # اذا تعدت الحد الايسر
+        if x<=-415:
             score.R_scoer += 1
             score.update_score()
             self.goto(0,0)
+            self.x_move = 1
+            self.y_move = -1
+
+        # اذا ضربت بالمضارب
+        if (self.distance(L_paddle)<=50 and x<=-360) or (self.distance(R_paddle)<=50 and x>=360 ) :
             self.x_move *= -1
 
+            if self.x_move > 0:
+                self.x_move += 1
+            else:
+                self.x_move -= 1
 
-  
-
-        if (self.distance(L_paddle)<=50 and x<=-360) or (self.distance(R_paddle)<=50 and x>=360 ) :
-            self.x_move *= -1.2
-            self.y_move *= -1.2
-
-            
+            if self.y_move > 0:
+                self.y_move += 1
+            else:
+                self.y_move -= 1
+           
         self.goto(self.xcor()+self.x_move, self.ycor()+self.y_move)
     
